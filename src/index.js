00a83996aed80fe6754a28b2912a898d3b3c0144
daemon9999 <murad.mesimli.99@gmail.com>
2022-12-25 00:20:59 +0400

    routes fixed

--- src/index.js
+++ src/index.js
@@ -4,12 +4,12 @@ import App from "./App";
 import "./tailwind.css";
 import store from "store";
 import { Provider } from "react-redux";
-import { BrowserRouter } from "react-router-dom";
+import {  HashRouter } from "react-router-dom";
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <Provider store={store}>
-    <BrowserRouter>
+    <HashRouter>
       <App />
-    </BrowserRouter>
+    </HashRouter>
   </Provider>
 );
