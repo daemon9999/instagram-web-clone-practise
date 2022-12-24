8a65808d5dbd77122571b27ab285c9b9bbfbb069
daemon9999 <murad.mesimli.99@gmail.com>
2022-12-25 01:18:48 +0400

    Changed

--- src/index.js
+++ src/index.js
@@ -4,12 +4,12 @@ import App from "./App";
 import "./tailwind.css";
 import store from "store";
 import { Provider } from "react-redux";
-import {  HashRouter } from "react-router-dom";
+import {  BrowserRouter } from "react-router-dom";
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <Provider store={store}>
-    <HashRouter>
+    <BrowserRouter>
       <App />
-    </HashRouter>
+    </BrowserRouter>
   </Provider>
 );
